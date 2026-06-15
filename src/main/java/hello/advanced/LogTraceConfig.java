58d7c4557a5dfb02e2e6fa061005fa600cb700ab
OllyDI <choiseungho11@hanmail.net>
2026-06-15 14:14:35 +0900

    26.06.15

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,6 +2,7 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace(); // singleton으로 스프링 빈 등록
+//        return new FieldLogTrace(); // Singleton 스프링 빈 등록
+        return new ThreadLocalLogTrace(); // Singleton 스프링 빈 등록
     }
 }

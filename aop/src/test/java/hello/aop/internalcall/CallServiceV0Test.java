bab086984fc7a957076db71615eb5afba397bac4
OllyDI <choiseungho11@hanmail.net>
2026-08-18 17:39:02 +0900

    26.08.18

---
+++ aop/src/test/java/hello/aop/internalcall/CallServiceV0Test.java
@@ -0,0 +1,33 @@
+package hello.aop.internalcall;
+
+import hello.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 실제 대상 객체 내부에서 내부 메서드 호출 시 AOP 적용 X
+ */
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV0Test {
+
+    @Autowired
+    CallServiceV0 callService;
+
+    @Test
+    void external() {
+        callService.external();
+    }
+
+    @Test
+    void internal() {
+        callService.internal();
+    }
+}
